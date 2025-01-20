4322d9a6dd08c852604c0c977745366eefab2102
danirssx <rossdaniel5018@gmail.com>
2025-01-20 18:45:38 -0400

    Zod schema added to react

--- src/components/formulario-reserva.jsx
+++ src/components/formulario-reserva.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { postReserva } from "../helpers/api";
+import { z } from "zod";
+
+const reservaSchema = z.object({
+  nombre: z.string().min(1, { message: "El nombre es obligatorio" }),
+  nota: z.string().optional(),
+  email: z.string().email({ message: "Debe ser un email válido" }),
+  telefono: z.string().min(6, { message: "Ingresa un teléfono válido" }),
+  fecha: z.string().min(1, { message: "La fecha es obligatoria" }),
+  hora: z.string().min(1, { message: "La hora es obligatoria" }),
+});
 
 function FormularioReserva() {
   const [reserva, setReserva] = useState({
@@ -12,12 +22,28 @@ function FormularioReserva() {
     hora: Date.now(),
   });
 
+  const [errors, setErrors] = useState({});
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await postReserva(reserva);
+    try {
+      reservaSchema.parse(reserva);
 
-    response && alert("Reserva guardada con exito");
+      const response = await postReserva(reserva);
+      if (response) alert("Reserva guardada con exito");
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        const fieldErrors = {};
+        err.issues.forEach((issue) => {
+          const fieldName = issue.path[0];
+          fieldErrors[fieldName] = issue.message;
+        });
+        setErrors(fieldErrors);
+      } else {
+        console.error(err);
+      }
+    }
   };
 
   return (
@@ -27,23 +53,26 @@ function FormularioReserva() {
         type="text"
         value={reserva.nombre}
         onChange={(e) => setReserva({ ...reserva, nombre: e.target.value })}
-        className="rounded-md border"
+        className="rounded-md border text-md text-gray-700"
       />
+      {errors.nombre && <p className="text-red-500 text-md">{errors.nombre}</p>}
 
       <label>Nota:</label>
       <textarea
         value={reserva.nota}
         onChange={(e) => setReserva({ ...reserva, nota: e.target.value })}
-        className="rounded-md border"
+        className="rounded-md border text-md text-gray-700"
       />
+      {errors.nota && <p className="text-red-500 text-md">{errors.nota}</p>}
 
       <label>Email:</label>
       <input
         type="text"
         value={reserva.email}
         onChange={(e) => setReserva({ ...reserva, email: e.target.value })}
-        className="rounded-md border"
+        className="rounded-md border text-md text-gray-700"
       />
+      {errors.email && <p className="text-red-500 text-md">{errors.email}</p>}
 
       <label>Telefono:</label>
       <input
@@ -55,8 +84,11 @@ function FormularioReserva() {
             telefono: e.target.value,
           })
         }
-        className="rounded-md border"
+        className="rounded-md border text-md text-gray-700"
       />
+      {errors.telefono && (
+        <p className="text-red-500 text-md">{errors.telefono}</p>
+      )}
 
       <label>Fecha:</label>
       <input
@@ -68,8 +100,9 @@ function FormularioReserva() {
             fecha: e.target.value,
           })
         }
-        className="rounded-md border"
+        className="rounded-md border text-md text-gray-700"
       />
+      {errors.fecha && <p className="text-red-500 text-md">{errors.fecha}</p>}
 
       <label>Hora:</label>
       <input
@@ -81,8 +114,9 @@ function FormularioReserva() {
             hora: e.target.value,
           })
         }
-        className="rounded-md border"
+        className="rounded-md border text-md text-gray-700"
       />
+      {errors.hora && <p className="text-red-500 text-md">{errors.hora}</p>}
 
       <button
         type="submit"
